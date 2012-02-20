926441cd4fe156dccb17b3a99f35903070b5498d
voodoo144@gmail.com <voodoo144@gmail.com@28de448a-68cd-b4d2-1883-544763f86ace>
2012-02-20 08:05:16 +0000

    Some exception handle

--- org.pageclass.genarator/src/main/java/org/pageclass/generator/Chains.java
+++ org.pageclass.genarator/src/main/java/org/pageclass/generator/Chains.java
@@ -8,24 +8,40 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 public class Chains {
 	public static HtmlPage doAction(String s, HtmlPage page) throws IOException {
 		XmlManager xml = new XmlManager();
-		HtmlPage final_page=null;
+		HtmlPage final_page = null;
 		int action_count = xml.getSettings(
 				"//page[@value='" + s + "']/actions/action").size();
-		for (int i = 1; i < action_count+1; i++) {
-			if (xml.getSettings(
-					"//page[@value='" + s + "']/actions/action[" + i
-							+ "]/@type").get(0).equals("fill")) {
-			    HtmlInput el=(HtmlInput) page.getFirstByXPath(xml.getSettings("//page[@value='" + s + "']/actions/action["+ i + "]/@path").get(0));
-				el.setValueAttribute(xml.getSettings("//page[@value='" + s + "']/actions/action["+ i + "]/@text").get(0));
+		try {
+			for (int i = 1; i < action_count + 1; i++) {
+				if (xml.getSettings(
+						"//page[@value='" + s + "']/actions/action[" + i
+								+ "]/@type").get(0).equals("fill")) {
+					HtmlInput el = (HtmlInput) page.getFirstByXPath(xml
+							.getSettings(
+									"//page[@value='" + s
+											+ "']/actions/action[" + i
+											+ "]/@path").get(0));
+					el.setValueAttribute(xml.getSettings(
+							"//page[@value='" + s + "']/actions/action[" + i
+									+ "]/@text").get(0));
+				}
+
+				if (xml.getSettings(
+						"//page[@value='" + s + "']/actions/action[" + i
+								+ "]/@type").get(0).equals("click")) {
+					HtmlElement el = (HtmlElement) page.getFirstByXPath(xml
+							.getSettings(
+									"//page[@value='" + s
+											+ "']/actions/action[" + i
+											+ "]/@path").get(0));
+					final_page = el.click();
+				}
 			}
-			if (xml.getSettings(
-					"//page[@value='" + s + "']/actions/action[" + i
-							+ "]/@type").get(0).equals("click")) {
-				HtmlElement el=(HtmlElement) page.getFirstByXPath(xml.getSettings("//page[@value='" + s + "']/actions/action["+ i + "]/@path").get(0));
-				final_page=el.click();
-			}
+		} catch (Exception e) {
+			System.out.println("Can not find element by given xpath");
 		}
 		return final_page;
+
 	}
 
 }
